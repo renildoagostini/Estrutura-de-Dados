1bb487004080a162e6f8d9d254736aed5025736e
Renildo Ribeiro Agostini <renildoribeiroagostini@gmail.com>
2024-11-13 15:23:59 -0300

    Semana 12

---
+++ Semana 12/Lista simplesmente Encadeada/Parte 1/script.js
@@ -0,0 +1,58 @@
+//Classe que cria um nó
+class No{
+    constructor(elemento){
+        this.elemento = elemento;
+        this.proximo = null;
+    }
+}
+
+//Classe que cria uma lista e suas funções
+class ListaEncadeada{
+    constructor(){
+        this.cabeca = null;
+        this.tamanho = 0;
+    }
+
+    //Inserir elementos
+    adicionar(elemento){
+        const novoNo = new No(elemento);
+
+        if(this.cabeca === null){
+            this.cabeca = novoNo;
+        }
+        else{
+            let atual = this.cabeca;
+
+            while(atual.proximo !== null){
+                atual = atual.proximo;
+            }
+            atual.proximo = novoNo;
+        }
+        this.tamanho++;
+    }
+    adicionar(posicao, elemento){
+        if(posicao >= 0 && posicao <= this.tamanho){
+            const novoNo = new No(elemento);
+
+            if(posicao === 0){
+                novoNo.proximo = this.cabeca;
+                this.cabeca = novoNo;
+            }else{
+                let atual = this.cabeca;
+                let anterior = null;
+                let indice = 0;
+
+                while(indice < posicao){
+                    anterior = atual;
+                    atual = atual.proximo;
+                    indice++;
+                }
+                novoNo.proximo = atual;
+                anterior.proximo = novoNo;
+            }
+            this.tamanho++;
+            return true;
+        }
+        return false;
+    }
+}
